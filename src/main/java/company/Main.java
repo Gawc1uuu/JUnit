b65264af0be56b391eabe4974fc7735ce836f67c
kamilapawluk <pawlukkamila08@gmail.com>
2023-08-10 12:08:35 +0200

    removed comments

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -49,32 +49,5 @@ public class Main {
 //        znalezc developera co ma najwiecej/najmniej projektow.
 //        znalezc developera co pracuje w wiecej niz x firm
 ////         metody projekty
-//        znalezc projekt ktory ma najwiecej developerow
-//        znalezc projekty danego Managera
-//        Developer developer = new Developer("Robin", "Hood", 80, 80, LocalDate.of(1995, 3, 15), Sex.MALE);
-//        Developer developer1 = new Developer("Meggie", "Doe", 85, 85, LocalDate.of(1993, 7, 24), Sex.FEMALE);
-//        Developer developer2 = new Developer("Martha", "Brown", 90, 90, LocalDate.of(1999, 9, 1), Sex.FEMALE);
-//        Developer developer3 = new Developer("Arnold", "Smith", 95, 95, LocalDate.of(1997, 2, 28), Sex.MALE);
-//        Manager manager = new Manager("Roberta", "Johannsen", 100, 100, LocalDate.of(1990, 12, 3), Sex.FEMALE);
-//        Manager manager1 = new Manager("Kristin", "Nilsen", 110, 105, LocalDate.of(2000, 1, 7), Sex.FEMALE);
-//        Company c1 = new Company("Samsung", LocalDate.of(2007, 2, 9));
-//        Company c2 = new Company("Apple", LocalDate.of(1984, 11, 13));
-//        c1.hireEmployee(developer);
-//        c1.hireEmployee(developer1);
-//        c1.hireEmployee(developer2);
-//        c1.hireEmployee(manager);
-//        c2.hireEmployee(developer1);
-//        c2.hireEmployee(developer3);
-//        c2.hireEmployee(manager1);
-//        System.out.println(EmployeeService.returnBestPaidWorkers(Employee.getExtension()));
-//        System.out.println(EmployeeService.returnTopNBestPaidWorkers(Employee.getExtension(), 3));
-//        System.out.println(EmployeeService.sortEmployeesBySalary(Employee.getExtension()));
-//        System.out.println(EmployeeService.findTheWorstPaidEmployees(Employee.getExtension(), 2));
-//        System.out.println(EmployeeService.findAverageSalary(Employee.getExtension()));
-//        System.out.println(EmployeeService.findTheOldestEmployee(Employee.getExtension()));
-//        System.out.println(EmployeeService.findTheYoungestEmployee(Employee.getExtension()));
-//        System.out.println(EmployeeService.returnAverageAgeOfEmployees(Employee.getExtension()));
-//        System.out.println(EmployeeService.returnThMostPopularSexWithinTheEmployees(Employee.getExtension()));
-//
     }
 }
